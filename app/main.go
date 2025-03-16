867a4229d770ce12b34f8df4366413bc5b10fbfd
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-03-16 12:06:16 +0530

    test

--- app/main.go
+++ app/main.go
@@ -26,66 +26,85 @@ func main() {
 	}
 	fmt.Printf("Read %d bytes: \n", n)
 
-	messageSize := make([]byte, 4)
-	binary.BigEndian.PutUint32(messageSize, 0)
-
 	response := []byte{}
+	apiVersion := request[6:8]
 
-	// // Message_size
+	// Message_size
 	// _, err = conn.Write(request[0:4])
 	// if err != nil {
 	// 	log.Fatalf("1:%v", err.Error())
 	// }
 
-	// Correlation ID
-	response = append(response, request[8:12]...)
+	apiVer := binary.BigEndian.Uint16(apiVersion)
 
-	// error_code
-	errorCode := make([]byte, 2)
-	binary.BigEndian.PutUint16(errorCode, 0)
-	response = append(response, errorCode...)
+	switch {
+	case apiVer >= 0 && apiVer < 5:
 
-	response = append(response, 2)
+		// Correlation ID
+		response = append(response, request[8:12]...)
 
-	// api keys
-	apiKeyIndex := make([]byte, 2)
-	binary.BigEndian.PutUint16(apiKeyIndex, 18)
-	response = append(response, apiKeyIndex...)
+		// error_code
+		errorCode := make([]byte, 2)
+		binary.BigEndian.PutUint16(errorCode, 0)
+		response = append(response, errorCode...)
 
-	apiMin := make([]byte, 2)
-	binary.BigEndian.PutUint16(apiMin, 3)
-	response = append(response, apiMin...)
+		response = append(response, 2)
 
-	apiMax := make([]byte, 2)
-	binary.BigEndian.PutUint16(apiMax, 4)
-	response = append(response, apiMax...)
+		// api keys
+		apiKeyIndex := make([]byte, 2)
+		binary.BigEndian.PutUint16(apiKeyIndex, 18)
+		response = append(response, apiKeyIndex...)
 
-	response = append(response, 0)
+		apiMin := make([]byte, 2)
+		binary.BigEndian.PutUint16(apiMin, 3)
+		response = append(response, apiMin...)
 
-	//trottle time
-	tt := make([]byte, 4)
-	binary.BigEndian.PutUint32(tt, 0)
-	response = append(response, tt...)
+		apiMax := make([]byte, 2)
+		binary.BigEndian.PutUint16(apiMax, 4)
+		response = append(response, apiMax...)
 
-	response = append(response, 0)
+		response = append(response, 0)
 
-	responseMessageSize := len(response)
+		//trottle time
+		tt := make([]byte, 4)
+		binary.BigEndian.PutUint32(tt, 0)
+		response = append(response, tt...)
 
-	responseMessageArray := make([]byte, 4)
-	binary.BigEndian.PutUint32(responseMessageArray, uint32(responseMessageSize))
+		response = append(response, 0)
 
-	_, err = conn.Write(responseMessageArray)
-	if err != nil {
-		log.Fatalf("1:%v", err.Error())
+		responseMessageSize := len(response)
+
+		responseMessageArray := make([]byte, 4)
+		binary.BigEndian.PutUint32(responseMessageArray, uint32(responseMessageSize))
+
+		_, err = conn.Write(responseMessageArray)
+		if err != nil {
+			log.Fatalf("1:%v", err.Error())
+		}
+
+		_, err = conn.Write(response)
+		if err != nil {
+			log.Fatalf("1:%v", err.Error())
+		}
+	default:
+		response = append(response, request[8:12]...)
+		errorCode := make([]byte, 2)
+		binary.BigEndian.PutUint16(errorCode, 35)
+		response = append(response, errorCode...)
+		responseMessageSize := len(response)
+
+		responseMessageArray := make([]byte, 4)
+		binary.BigEndian.PutUint32(responseMessageArray, uint32(responseMessageSize))
+
+		_, err = conn.Write(responseMessageArray)
+		if err != nil {
+			log.Fatalf("1:%v", err.Error())
+		}
+		_, err = conn.Write(response)
+		if err != nil {
+			log.Fatalf("1:%v", err.Error())
+		}
 	}
-
-	fmt.Println(responseMessageSize)
-
-	_, err = conn.Write(response)
-	if err != nil {
-		log.Fatalf("1:%v", err.Error())
-	}
-
 }
 
 // ApiVersions Response (Version: 4) => error_code [api_keys] throttle_time_ms TAG_BUFFER
