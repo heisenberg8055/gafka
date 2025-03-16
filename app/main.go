479ebd59c0923db9756d70af5067f89ba8a7ce5c
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-03-16 22:00:46 +0530

    test

--- app/main.go
+++ app/main.go
@@ -34,7 +34,7 @@ func handleConnection(conn net.Conn) {
 		}
 		fmt.Printf("Read %d bytes: \n", n)
 
-		fmt.Printf("test:%v", (request[0:54]))
+		fmt.Printf("test:%v", string(request[0:54]))
 
 		response := []byte{}
 		apiKey := request[4:6]
